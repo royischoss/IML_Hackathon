a3c0d39da2bdbd5a55d6edc745b6e91734f4741f
Barak Ramni <brkram@cs.huji.ac.il>
2021-06-02 22:08:38 +0300

    test and train df

--- split_data.py
+++ split_data.py
@@ -1,3 +1,12 @@
-import pandas
-import numpy as np
-# hello
+import pandas as pd
+
+DATA_PATH = "Dataset/dataset_crimes.csv"
+TRAIN_VALIDATE = 0.9
+RANDOM_SEED = 207
+
+df = pd.read_csv(DATA_PATH)
+train=df.sample(frac=TRAIN_VALIDATE,random_state=RANDOM_SEED) #random state is a seed value
+test=df.drop(train.index)
+
+train.to_csv('Dataset/train_validate.csv')
+test.to_csv('Dataset/test.csv')
