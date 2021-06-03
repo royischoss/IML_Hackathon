fb2dbfb9b4d57ff5d92c06dc30aff4e3cb5861c2
Nitzan <nitzan.barz@gmail.com>
2021-06-03 15:12:01 +0300

    Updates2

--- CreateDataframe.py
+++ CreateDataframe.py
@@ -5,43 +5,7 @@ from _datetime import datetime
 crimes_dict = {0: 'BATTERY', 1: 'THEFT', 2: 'CRIMINAL DAMAGE',
                3: 'DECEPTIVE PRACTICE', 4: 'ASSAULT'}
 
-
-def create_4_df_splits_processed():
-    """
-    Creates and returns 4 data splits (original_full, train, validation, test)
-    after preprocessing
-    """
-    original_full = preprocess_data("Dataset/dataset_crimes.csv")
-    train = preprocess_data("Dataset/train.csv")
-    validation = preprocess_data("Dataset/validate.csv")
-    test = preprocess_data("Dataset/test.csv")
-    return original_full, train, validation, test
-
-
-def create_4_df_splits_raw():
-    """
-    Creates and returns 4 data splits (original_full, train, validation, test)
-    using raw data *without* preprocessing
-    """
-    original_full = pd.read_csv("Dataset/dataset_crimes.csv", index_col=0)
-    train = pd.read_csv("Dataset/train.csv", index_col=0)
-    validation = pd.read_csv("Dataset/validate.csv", index_col=0)
-    test = pd.read_csv("Dataset/test.csv", index_col=0)
-    return original_full, train, validation, test
-
-
-def preprocess_data(path):
-    """
-    Creates and returns a pandas dataframe from given file path, after
-    preprocessing
-    """
-    df = pd.read_csv(path, index_col=0)
-
-    add_primary_code_col(df)
-    date_process(df)
-    df = dummies(df)
-    drop(df)
-    c_names = ['ID', 'Case Number', 'Date', 'Block', 'Arrest', 'Domestic', 'Beat', 'District', 'Ward', 'Community Area',
+c_names = ['ID', 'Case Number', 'Date', 'Block', 'Arrest', 'Domestic', 'Beat', 'District', 'Ward', 'Community Area',
                'X Coordinate', 'Y Coordinate', 'Year', 'Latitude', 'Longitude', 'Location', 'Primary Code',
                'Location Description_ABANDONED BUILDING', 'Location Description_AIRCRAFT',
                'Location Description_AIRPORT BUILDING NON-TERMINAL - NON-SECURE AREA',
@@ -102,6 +66,42 @@ def preprocess_data(path):
                'Location Description_VEHICLE NON-COMMERCIAL', 'Location Description_WAREHOUSE', 'Hour Code_0',
                'Hour Code_1', 'Hour Code_2', 'day_of_week_Friday', 'day_of_week_Monday', 'day_of_week_Saturday',
                'day_of_week_Sunday', 'day_of_week_Thursday', 'day_of_week_Tuesday', 'day_of_week_Wednesday', 'other']
+
+def create_4_df_splits_processed():
+    """
+    Creates and returns 4 data splits (original_full, train, validation, test)
+    after preprocessing
+    """
+    original_full = preprocess_data("Dataset/dataset_crimes.csv")
+    train = preprocess_data("Dataset/train.csv")
+    validation = preprocess_data("Dataset/validate.csv")
+    test = preprocess_data("Dataset/test.csv")
+    return original_full, train, validation, test
+
+
+def create_4_df_splits_raw():
+    """
+    Creates and returns 4 data splits (original_full, train, validation, test)
+    using raw data *without* preprocessing
+    """
+    original_full = pd.read_csv("Dataset/dataset_crimes.csv", index_col=0)
+    train = pd.read_csv("Dataset/train.csv", index_col=0)
+    validation = pd.read_csv("Dataset/validate.csv", index_col=0)
+    test = pd.read_csv("Dataset/test.csv", index_col=0)
+    return original_full, train, validation, test
+
+
+def preprocess_data(path):
+    """
+    Creates and returns a pandas dataframe from given file path, after
+    preprocessing
+    """
+    df = pd.read_csv(path, index_col=0)
+    add_primary_code_col(df)
+    date_process(df)
+    df = dummies(df)
+    drop(df)
+    drop_task_1(df)
     d = {}
     for c in c_names:
         d[c] = [0]
@@ -163,6 +163,8 @@ def drop(df):
     df.drop(df.columns[df.columns.str.contains('unnamed', case=False)], axis=1, inplace=True)
     df.drop('Primary Type', axis=1, inplace=True)
     df.drop('hour', axis=1, inplace=True)
+    df.drop('Case Number', axis=1, inplace=True)
+    df.drop('Updated On', axis=1, inplace=True)
 
 def drop_task_1(df):
     """
@@ -171,7 +173,8 @@ def drop_task_1(df):
     df.drop('IUCR', axis=1, inplace=True)
     df.drop('Description', axis=1, inplace=True)
     df.drop('FBI Code', axis=1, inplace=True)
-    df.drop('Updated On', axis=1, inplace=True)
+
+
 
 def split_features_and_labels(df):
     """
